9fc0ef11f780a543814a0525940ad6fde6ade54f
anteroserrano99 <serranoantero13@gmail.com>
2020-12-27 15:51:26 +0100

    Added an black and white image for further visualization, and extreme values have been smoothed

--- src/test/kotlin/com/perlin/noise/terraingenerator/DiamondSquareAlgorithmTest.kt
+++ src/test/kotlin/com/perlin/noise/terraingenerator/DiamondSquareAlgorithmTest.kt
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
+import java.awt.geom.AffineTransform
+import java.awt.image.AffineTransformOp
+import java.awt.image.BufferedImage
 import java.io.File
+import javax.imageio.ImageIO
 import kotlin.random.Random
 
 @SpringBootTest
@@ -12,12 +16,12 @@ class DiamondSquareAlgorithmTest {
 
 
     val logger: Logger = LoggerFactory.getLogger(DiamondSquareAlgorithmTest::class.java)
-    var RANDOM_MODIFIER = 1F
 
-    val X_MAX = 16
-    val Y_MAX = 16
-    var RANDOM_SMOTHER = 0.01F
-    var grid: Array<IntArray> = Array(X_MAX + 1) { IntArray(Y_MAX + 1 ) { -1 } };
+
+    val X_MAX = 1024
+    val Y_MAX = 1024
+    var RANDOM_SMOTHER = 0.0
+    var grid: Array<DoubleArray> = Array(X_MAX + 1) { DoubleArray(Y_MAX + 1 ) { -1.0 } };
 
     @Test
     fun diamonSquareTest() {
@@ -26,25 +30,27 @@ class DiamondSquareAlgorithmTest {
         //PODRIA HACER UNA COMPROBACION DE TODOS LOS VALORES SI EXISTEN
         var suma = 0f;
         var minimo = 255f;
-        var maximo = 0f;
+        var maximo = 0f;generateRandomValue(RANDOM_SMOTHER)
 
 
-        grid[0][0] = generateRandomValue(RANDOM_MODIFIER)
-        grid[X_MAX][0] = generateRandomValue(RANDOM_MODIFIER)
-        grid[0][Y_MAX] = generateRandomValue(RANDOM_MODIFIER)
-        grid[X_MAX][Y_MAX] = generateRandomValue(RANDOM_MODIFIER)
+        grid[0][0] = generateRandomValue(RANDOM_SMOTHER)
+        grid[X_MAX][0] = generateRandomValue(RANDOM_SMOTHER)
+        grid[0][Y_MAX] = generateRandomValue(RANDOM_SMOTHER)
+        grid[X_MAX][Y_MAX] = generateRandomValue(RANDOM_SMOTHER)
         //punto central para la prueba con el diamond step
 //        grid[X_MAX/2][Y_MAX/2] = generateRandomValue(RANDOM_MODIFIER)
-        writeToFile("START")
+//        writeToFile("START")
 
         var corner00 = grid[0][0]
         var cornerX0 = grid[X_MAX][0]
         var corner0Y = grid[0][Y_MAX]
         var cornerXY = grid[X_MAX][Y_MAX]
-        while (grid[1][0] == -1) {
+        while (grid[1][0] == -1.0) {
             diamondStep(0, 0, X_MAX, Y_MAX)
             logger.info("BUCLE")
         }
+        writeToFile("WRITE")
+        createImage()
         logger.info("\nC00 $corner00 \n CX0 $cornerX0 \n C0Y $corner0Y \n CXY $cornerXY")
 
 
@@ -66,16 +72,20 @@ class DiamondSquareAlgorithmTest {
     }
 
 
-    fun generateRandomValue(randomModifier: Float): Int {
+    fun generateRandomValue(randomModifier: Double): Double {
 
         var sign = Random.nextBoolean();
 
-        var modifier = 32f * randomModifier;
+        var modifier = 255f * randomModifier;
         if (sign) modifier *= -1
 
-        var randomNumber = 32f;
+        var randomNumber = 127f;
+        var value = Random.nextDouble()
+//        logger.info("$value")
+        if (value < 0.1) value += 0.15
+        if (value > 0.9) value -= 0.15
 
-        return (Random.nextFloat() * (randomNumber + modifier)).toInt()
+        return value
     }
 
 
@@ -90,11 +100,13 @@ class DiamondSquareAlgorithmTest {
         var center = grid[centerx][centery]
 
         //TODO LA CONDICION DE SALIDA ESTA JODIDA YA QUE SE QUEDA EN BUCLE INFINITO UNA VEZ LLEGADO AL 3 BUCLE
-        if (center == -1) {
+        if (center == -1.0) {
             //ASIGNAMOS VALOR AL CENTRO
 
-            grid[centerx][centery] = ((c1 + c2 + c3 + c4) / 4) + generateRandomValue(RANDOM_SMOTHER)
-            writeToFile("DIAMOND")
+//            grid[centerx][centery] = ((c1 + c2 + c3 + c4) / 4) + generateRandomValue(RANDOM_SMOTHER)
+            grid[centerx][centery] = ((c1 + c2 + c3 + c4 + generateRandomValue(RANDOM_SMOTHER)) / 4)
+
+//            writeToFile("DIAMOND")
             //TODO REVISAR DISTANCIA DE NODO
             squareStep(centerx,centery,-(minX-centerx))
             //LLAMAMOS AQUI AL SQUARE STEP
@@ -143,7 +155,8 @@ class DiamondSquareAlgorithmTest {
                 var rowString = ""
 
                 for (column in row) {
-                    var value: Char = if (column == -1) '-' else '#'
+                    //var value: Char = if (column == -1) '-' else '#'
+                    var value = column
                     rowString += "$value "
                 }
                 out.println(rowString)
@@ -162,10 +175,10 @@ class DiamondSquareAlgorithmTest {
 
 
         var adjacentNodes = 4;
-        var c1 = 0
-        var c2 = 0
-        var c3 = 0
-        var c4 = 0
+        var c1 = 0.0
+        var c2 = 0.0
+        var c3 = 0.0
+        var c4 = 0.0
 
 
         if ((centerX >= 0 && centerX < grid.size) && (centerY-nodeDistance >= 0 && centerY-nodeDistance < grid.size)) c1 = grid[centerX][centerY-nodeDistance] else adjacentNodes--
@@ -173,11 +186,38 @@ class DiamondSquareAlgorithmTest {
         if ((centerX+nodeDistance >= 0 && centerX+nodeDistance < grid.size) && (centerY >= 0 && centerY <= grid.size)) c3 = grid[centerX+nodeDistance][centerY] else adjacentNodes--
         if ((centerX >= 0 && centerX < grid.size) && (centerY+nodeDistance >= 0 && centerY+nodeDistance < grid.size)) c4 = grid[centerX][centerY+nodeDistance] else adjacentNodes--
 
-        grid[centerX][centerY] = ((c1 + c2 + c3 + c4) / adjacentNodes) + generateRandomValue(RANDOM_SMOTHER)
-        writeToFile("SQUARE")
+        grid[centerX][centerY] = ((c1 + c2 + c3 + c4 + generateRandomValue(RANDOM_SMOTHER))  / adjacentNodes)
+        //writeToFile("SQUARE")
 
 
     }
+
+    fun createImage(){
+
+        var filename = "src/test/resources/FileDebug/noise.png"
+
+        var image = BufferedImage(X_MAX, Y_MAX, BufferedImage.TYPE_INT_RGB)
+        for(x in 0 until X_MAX){
+            for (y in 0 until X_MAX){
+                val value = grid[x][y]
+                var rbg = 0x010101 *  ((value + 1) * 127.5).toInt()
+                image.setRGB(x,y, rbg)
+            }
+        }
+        var at: AffineTransform = AffineTransform()
+        at.scale(4.0,4.0)
+        var scaleOp: AffineTransformOp = AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR)
+        var afterImage = BufferedImage(X_MAX, Y_MAX, BufferedImage.TYPE_INT_RGB)
+        afterImage = scaleOp.filter(image,afterImage)
+
+        ImageIO.write(afterImage,"png",File(filename))
+
+
+    }
+
+
+
+
 
 
 }
